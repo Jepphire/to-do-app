c51fc15b773d20ae44fba2fcf2453033a68e2f94
Jepphire <90650560+Jepphire@users.noreply.github.com>
2022-05-05 20:35:48 -0500

    implemented material navigation containers and header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './header/header.component';
+import { TodoComponent } from './todo/todo.component';
+import { TodoListComponent } from './todo/todo-list/todo-list.component';
+import { TodoDetailComponent } from './todo/todo-detail/todo-detail.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './shared/modules/material/material.module';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    TodoComponent,
+    TodoListComponent,
+    TodoDetailComponent,
+    SidebarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
